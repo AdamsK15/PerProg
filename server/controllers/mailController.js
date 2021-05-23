0266e3d8bad520e1dd23ec85a80ca2514a50d4e6
unknown <kevadams15@gmail.com>
2021-05-22 22:12:37 -0500

    minor update

--- server/controllers/mailController.js
+++ server/controllers/mailController.js
@@ -1,44 +1,31 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer')
 
 const { EMAIL, PASSWORD } = process.env
 
 module.exports = {
     email: async (req, res) => {
-        const { name, message, email, title, image } = req.body
-
+        const { name, message, email, title } = req.body
+        console.log(email)
         try {
-            //invoke the createTransport function passing in your email information. 
             let transporter = nodemailer.createTransport({
-                service: 'gmail',
+                host: 'smtp.ethereal.email',
+                port: 587,
+                secure: false,
                 auth: {
                     user: EMAIL,
                     pass: PASSWORD
                 }
             });
-
-            //invoke the sendMail function with the info in the email
             let info = await transporter.sendMail({
-                from: `'${name}' <${email}>`, //This will show up when you go into the email
+                from: `'${name}' <${email}>`,
                 to: EMAIL,
-                subject: title, //This will show on the subject of the email
-                text: message, //for clients with plaintext support only
-                html: `<div>${message}<div> 
-              <img src="cid:unique@nodemailer.com"/>`,
-                attachments: [
-                    { //this is the attachment of the document
-                        filename: 'license.txt',
-                        path: 'https://raw.github.com/nodemailer/nodemailer/master/LICENSE'
-                    },
-                    { //this is the embedded image
-                        cid: 'unique@nodemailer.com', //same cid value as in the html img src
-                        path: image
-                    }
-                ]
-            }, (err, res) => {
+                subject: title,
+                text: message
+            }, (err, success) => {
                 if (err) {
                     console.log('err', err)
                 } else {
-                    console.log('res', res)
+                    console.log('res', success)
                     res.status(200).send(info)
                 }
             })
--- src/Components/Mailer.js
+++ src/Components/Mailer.js
@@ -34,15 +34,15 @@ class Mailer extends Component {
     render() {
         const { name, email, message, title, image } = this.state
         return (
-            <div>
-                <div>
-                    <h1>Email Kevon</h1>
-                    <input placeholder='title' type="text" name='title' value={title} onChange={this.handleInput} />
-                    <input placeholder='name' type="text" name='name' value={name} onChange={this.handleInput} />
-                    <input placeholder='email' type="text" name='email' value={email} onChange={this.handleInput} />
-                    <input placeholder='message' type="text" name='message' value={message} onChange={this.handleInput} />
-                    <input placeholder='image' type="text" name='image' value={image} onChange={this.handleInput} />
-                    <button onClick={this.handleSend}>Send</button>
+            <div style={styles.body}>
+                <div style={styles.form}>
+                    <h1 style={styles.header}>Email Kevon</h1>
+                    <input style={styles.input} placeholder='title' type="text" name='title' value={title} onChange={this.handleInput} />
+                    <input style={styles.input} placeholder='name' type="text" name='name' value={name} onChange={this.handleInput} />
+                    <input style={styles.input} placeholder='email' type="text" name='email' value={email} onChange={this.handleInput} />
+                    <input style={styles.input} placeholder='message' type="text" name='message' value={message} onChange={this.handleInput} />
+                    {/* <input style={styles.input} placeholder='image' type="text" name='image' value={image} onChange={this.handleInput} /> */}
+                    <button style={styles.button} onClick={this.handleSend}>Send</button>
                 </div>
             </div>
         )
@@ -50,3 +50,46 @@ class Mailer extends Component {
 }
 
 export default Mailer;
+
+
+const styles = {
+    body: {
+        background: 'lightgrey',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        background: '#00000090',
+        width: 500,
+        alignItems: 'center',
+        height: 500,
+        justifyContent: 'space-evenly',
+        borderRadius: 10
+    },
+    header: {
+        fontSize: 60,
+        margin: 0,
+        color: 'white',
+        letterSpacing: '0.07em',
+        fontWeight: 'bold'
+    },
+    input: {
+        width: 450,
+        height: 50,
+        fontSize: 35,
+        outline: 'none'
+    },
+    button: {
+        width: 200,
+        height: 45,
+        borderRadius: 10,
+        background: 'green',
+        fontSize: 35,
+        fontWeight: 'bold',
+        letterSpacing: '0.07em'
+    }
+}
